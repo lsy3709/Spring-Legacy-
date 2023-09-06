eafde5858f08ee261c114140d794a57774b69ae6
MKKim <139408936+designexit@users.noreply.github.com>
2023-09-06 10:27:41 +0900

    9/6 10:27 push

---
+++ pro19/src/com/spring/kmk0906/KMKTest.java
@@ -0,0 +1,19 @@
+package com.spring.kmk0906;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+
+// 자바의 특성상, 메인 메서드에서 실행을 합니다. 
+public class KMKTest {
+
+	// 메인 메서드에서, 
+	public static void main(String[] args) {
+
+	System.out.println("Git test 김민경");
+		
+		//homework브랜치 입니다.
+		
+	}
+
+}
