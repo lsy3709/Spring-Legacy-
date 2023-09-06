58d98d572d20a0cf004998ed135d773e39f4b990
Lian <lcb_faust@naver.com>
2023-09-06 10:36:00 +0900

    gittest

---
+++ pro19/src/com/spring/scb0906/scbTest.java
@@ -0,0 +1,14 @@
+package com.spring.scb0906;
+
+
+// 자바의 특성상, 메인 메서드에서 실행을 합니다. 
+public class scbTest {
+
+	// 메인 메서드에서, 
+	public static void main(String[] args) {
+		System.out.println("테스트");
+		
+		
+	}
+
+}
