dd9753e87f2ce0dae6e5efce5a28266591db5ce0
손주원 <susorrow@naver.com>
2023-09-06 10:30:06 +0900

    com.spring.sjw0906 패키지 & SwjTest.java 추가

---
+++ pro19/src/com/spring/sjw/SjwTest.java
@@ -0,0 +1,14 @@
+package com.spring.sjw;
+
+import org.springframework.core.io.FileSystemResource;
+
+// 자바의 특성상, 메인 메서드에서 실행을 합니다. 
+public class SjwTest {
+
+	// 메인 메서드에서, 
+	public static void main(String[] args) {
+	System.out.println("테스트중입니다~");
+		
+	}
+
+}
