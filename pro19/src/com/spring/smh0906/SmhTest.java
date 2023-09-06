a773b6b029cfe36a6cc6b4b9259169858fe35962
송민하 <thd0071@naver.com>
2023-09-06 10:28:25 +0900

    깃 테스트 중 송민하

---
+++ pro19/src/com/spring/smh0906/SmhTest.java
@@ -0,0 +1,15 @@
+package com.spring.smh0906;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+
+// 자바의 특성상, 메인 메서드에서 실행을 합니다. 
+public class SmhTest {
+
+	// 메인 메서드에서, 
+	public static void main(String[] args) {
+	System.out.println("깃 테스트중 송민하");
+	}
+
+}
