4aeb1d79ece2116a31dd8ccd23c72c9717cd6f02
강정현 <life.us0410@gmail.com>
2023-09-06 16:35:48 +0900

    230906 강정현 과제제출

---
+++ pro20/src/com/spring/kjh0906/kjhTest.java
@@ -0,0 +1,11 @@
+package com.spring.kjh0906;
+
+public class kjhTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("°­Á¤Çö");
+	}
+
+}
